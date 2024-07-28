7b7e41af13567c7e4edc27f731f1edc1498d766d
stevenptran <tran.steven.p@gmail.com>
2024-07-28 17:27:12 -0500

    retry

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         {`I'm a data scientist who is particularly interested in machine learning models, specifically clustering models. I also enjoy Natural Language Processing 
-        (NLP). I am currently working on expanding my skillset to better myself in the frontend.`}
+        (NLP). I am currently working on expanding my skillset to better myself in the frontend. I graduated from Worcester Polytechnic Institute in Massachusetts in December 2023 with my M.S. in Data Science.`}
       </p>
       <div className="my-8">
         <BlogPosts />
